f3015de3ec363f15c8f4ff1f096cb3fc144c31fe
Alexander Smith <acsmith.dev@gmail.com>
2019-07-30 15:29:13 -0400

    Initial Commit

---
+++ LandmarkApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LandmarkApp
+//
+//  Created by Alexander Smith on 7/30/19.
+//  Copyright © 2019 Alexander Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
