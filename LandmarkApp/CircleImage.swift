fa34d8c0a5bb6a7ed2754f90f2e283158162c0a4
Alexander Smith <acsmith.dev@gmail.com>
2019-07-30 15:44:04 -0400

    Initial Build - Use MapKit

---
+++ LandmarkApp/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  LandmarkApp
+//
+//  Created by Alexander Smith on 7/30/19.
+//  Copyright © 2019 Alexander Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
